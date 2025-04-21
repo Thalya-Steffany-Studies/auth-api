8f211174ae214319758c8d8564937d8b6c1bebe0
Thalya Stéffany <thalyacoiimbra@gmail.com>
2025-04-21 11:06:59 -0300

    feat(config): adding 'callback' param to function connectDB

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
 
-const connectDB = async () => {
+const connectDB = async (callback) => {
   try {
     await mongoose.connect(process.env.DB_STRING_CONNECTION);
     console.log('✅ MongoDB conectado');
+    if(callback) callback()
   } catch (error) {
     console.error('❌ Erro ao conectar no MongoDB', error.message);
     process.exit(1);
